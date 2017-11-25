9664f9bec86a0135d7643e9cd1313cace37e34ce
choker <1027815252@qq.com>
2017-11-25 17:08:40 +0800

    [choker]修改主页

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-	echo "Hello Anige!";
-?>
+<!doctype html>
+<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title></title>
+	    <style>
+	    	html,body {
+	    		height: 100%;
+	    	}
+			#app {
+				width: 1366px;
+				height: 100%;
+				background-color: #ff0;
+				margin: 0 auto;
+			}
+	    </style>
+	</head>
+	<body>
+		<div id="app"></div>
+	</body>
+	<script src="./resource/js/require.js" defer async="true" data-main="./main.js"data-main="js/main"></script>
+</html>
