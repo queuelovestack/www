6acfab751e9d46613538d2913bb84da02853d253
周新 <charleszhou@123u.com>
2017-11-26 16:57:24 +0800

    【queuelovestack】添加导航块

--- index.php
+++ index.php
@@ -7,16 +7,35 @@
 	    	html,body {
 	    		height: 100%;
 	    	}
-			#app {
+			#home {
 				width: 1366px;
 				height: 100%;
-				background-color: #ff0;
+				background-color: #fff;
 				margin: 0 auto;
 			}
 	    </style>
+		<link rel="stylesheet" type="text/css" href="./resource/css/index.css">
 	</head>
 	<body>
-		<div id="app"></div>
+		<div id="home">
+			<div class="m-border-1 m-inner-1" style="height: auto; min-height: 300px;">
+				<div class="sub-grid1-wr">
+					<div class="sub-grid1">
+						<div class="sub-title">
+							<a>导航</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://www.baidu.com/" target="_blank">
+									<img alt="百度" src="http://www.baidu.com/favicon.ico">
+									<span class="title">百度</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
 	</body>
-	<script src="./resource/js/require.js" defer async="true" data-main="./main.js"data-main="js/main"></script>
+	<script src="./resource/js/require.js" defer async="true" data-main="./resource/js/main.js"data-main="js/main"></script>
 </html>
