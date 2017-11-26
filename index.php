baf0ce9ecc5e42b0ccd3d914c2604f0a9634fb98
周新 <charleszhou@123u.com>
2017-11-26 20:27:27 +0800

    【queuelovestack】导航块增添

--- index.php
+++ index.php
@@ -22,6 +22,88 @@
 				<div class="sub-grid1-wr">
 					<div class="sub-grid1">
 						<div class="sub-title">
+							<a>OJ</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://poj.org/" target="_blank">
+									<img alt="POJ" src="http://poj.org/poj.ico">
+									<span class="title">POJ</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://acm.hdu.edu.cn/" target="_blank">
+									<img alt="HDU" src="http://acm.hdu.edu.cn/favicon.ico">
+									<span class="title">HDU</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.nbuoj.com/" target="_blank">
+									<img alt="NBU" src="http://www.nbuoj.com/v8.8/Images/bg_block.jpg">
+									<span class="title">NBU</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1">
+						<div class="sub-title">
+							<a>手游</a>
+						</div>
+						<ul class="list clearfix">
+						</ul>
+					</div>
+					<div class="sub-grid1">
+						<div class="sub-title">
+							<a>网游</a>
+						</div>
+						<ul class="list clearfix">
+						</ul>
+					</div>
+					<div class="sub-grid1">
+						<div class="sub-title">
+							<a>视频</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://www.youku.com/" target="_blank">
+									<img alt="优酷" src="http://static.youku.com/v1.0.166/index/img/favicon.ico">
+									<span class="title">优酷</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.iqiyi.com/" target="_blank">
+									<img alt="爱奇艺" src="http://www.iqiyi.com/favicon.ico">
+									<span class="title">爱奇艺</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://v.qq.com/" target="_blank">
+									<img alt="腾讯视频" src="http://v.qq.com/favicon.ico">
+									<span class="title">腾讯视频</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.pptv.com/" target="_blank">
+									<img alt="PPTV" src="http://sr1.pplive.com/mcms/nav/images/favicon.ico">
+									<span class="title">PPTV</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.tudou.com/" target="_blank">
+									<img alt="土豆" src="http://www.tudou.com/favicon.ico">
+									<span class="title">土豆</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://tv.sohu.com/" target="_blank">
+									<img alt="搜狐视频" src="https://tv.sohu.com/favicon.ico">
+									<span class="title">搜狐视频</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1">
+						<div class="sub-title">
 							<a>导航</a>
 						</div>
 						<ul class="list clearfix">
@@ -29,6 +111,68 @@
 								<a href="http://www.baidu.com/" target="_blank">
 									<img alt="百度" src="http://www.baidu.com/favicon.ico">
 									<span class="title">百度</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1">
+						<div class="sub-title">
+							<a>博客</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="https://www.csdn.net/" target="_blank">
+									<img alt="CSDN" src="https://csdnimg.cn/public/favicon.ico">
+									<span class="title">CSDN</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.cnblogs.com/" target="_blank">
+									<img alt="博客园" src="https://common.cnblogs.com/favicon.ico">
+									<span class="title">博客园</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://blog.sina.com.cn/" target="_blank">
+									<img alt="新浪博客" src="http://blog.sina.com.cn/favicon.ico">
+									<span class="title">新浪博客</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://blog.163.com/" target="_blank">
+									<img alt="网易博客" src="http://blog.163.com/favicon.ico">
+									<span class="title">网易博客</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://blog.tianya.cn/" target="_blank">
+									<img alt="天涯博客" src="http://static.tianyaui.com/favicon.ico">
+									<span class="title">天涯博客</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.tom-z.cn/" target="_blank">
+									<img alt="TOM博客" src="http://www.tom-z.cn/wp-content/themes/zhigengniao/img/favicon.ico">
+									<span class="title">TOM博客</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://blog.sohu.com/" target="_blank">
+									<img alt="搜狐博客" src="http://blog.sohu.com/favicon.ico">
+									<span class="title">搜狐博客</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1">
+						<div class="sub-title">
+							<a>生活</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://www.12306.cn/mormhweb/" target="_blank">
+									<img alt="12306" src="http://www.12306.cn/mormhweb/images/favicon.ico">
+									<span class="title">12306</span>
 								</a>
 							</li>
 						</ul>
