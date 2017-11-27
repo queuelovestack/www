f31324720040119f1af2b7ee04e18ca63c74605b
周新 <charleszhou@123u.com>
2017-11-27 17:28:56 +0800

    【queuelovestack】拓展导航块

--- index.php
+++ index.php
@@ -133,48 +133,115 @@
 			</div>
 		</div>
 		<div id="home">
-			<div class="m-border-1 m-inner-1" style="height: auto; min-height: 300px;">
+			<div module="website" class="m-border-1 m-inner-1" style="height: auto; min-height: 300px;">
 				<div class="sub-grid1-wr">
-					<div class="sub-grid1">
+					<div class="sub-grid1"><!-- ABCD -->
 						<div class="sub-title">
-							<a>OJ</a>
+							<a>ABCD</a>
 						</div>
 						<ul class="list clearfix">
 							<li>
-								<a href="http://poj.org/" target="_blank">
-									<img alt="POJ" src="http://poj.org/poj.ico">
-									<span class="title">POJ</span>
+								<a href="http://www.acfun.tv/" target="_blank">
+									<img alt="AcFun" src="http://cdn.aixifan.com/ico/favicon.ico">
+									<span class="title">AcFun</span>
 								</a>
 							</li>
 							<li>
-								<a href="http://acm.hdu.edu.cn/" target="_blank">
-									<img alt="HDU" src="http://acm.hdu.edu.cn/favicon.ico">
-									<span class="title">HDU</span>
+								<a href="https://www.bilibili.com/" target="_blank">
+									<img alt="哔哩哔哩" src="https://static.hdslb.com/images/favicon.ico">
+									<span class="title">哔哩哔哩</span>
 								</a>
 							</li>
 							<li>
-								<a href="http://www.nbuoj.com/" target="_blank">
-									<img alt="NBU" src="http://www.nbuoj.com/v8.8/Images/bg_block.jpg">
-									<span class="title">NBU</span>
+								<a href="http://www.tucao.tv/" target="_blank">
+									<img alt="吐槽" src="./resource/images/ico/tucao_favicon.ico">
+									<span class="title">吐槽</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.dilidili.wang/" target="_blank">
+									<img alt="嘀哩嘀哩" src="http://www.dilidili.wang/favicon.ico">
+									<span class="title">嘀哩嘀哩</span>
 								</a>
 							</li>
 						</ul>
 					</div>
-					<div class="sub-grid1">
+					<div class="sub-grid1"><!-- 导航 -->
 						<div class="sub-title">
-							<a>手游</a>
+							<a>导航</a>
 						</div>
 						<ul class="list clearfix">
+							<li>
+								<a href="http://www.baidu.com/" target="_blank">
+									<img alt="百度" src="http://www.baidu.com/favicon.ico">
+									<span class="title">百度</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.moe123.net/" target="_blank">
+									<img alt="萌导航" src="http://www.moe123.net/favicon.ico">
+									<span class="title">萌导航</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://hao.360.cn/" target="_blank">
+									<img alt="360导航" src="https://hao.360.cn/favicon.ico">
+									<span class="title">360导航</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.hao123.com/" target="_blank">
+									<img alt="hao123" src="https://www.hao123.com/favicon.ico">
+									<span class="title">hao123</span>
+								</a>
+							</li>
 						</ul>
 					</div>
-					<div class="sub-grid1">
+					<div class="sub-grid1"><!-- 工具 -->
 						<div class="sub-title">
-							<a>网游</a>
+							<a>工具</a>
 						</div>
 						<ul class="list clearfix">
+							<li>
+								<a href="http://www.baidu.com/" target="_blank">
+									<img alt="QQ" src="http://www.baidu.com/favicon.ico">
+									<span class="title">QQ</span>
+								</a>
+							</li>
 						</ul>
 					</div>
-					<div class="sub-grid1">
+					<div class="sub-grid1"><!-- 生活 -->
+						<div class="sub-title">
+							<a>生活</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://www.12306.cn/mormhweb/" target="_blank">
+									<img alt="12306" src="http://www.12306.cn/mormhweb/images/favicon.ico">
+									<span class="title">12306</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.taobao.com/" target="_blank">
+									<img alt="淘宝网" src="https://www.taobao.com/favicon.ico">
+									<span class="title">淘宝网</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.tmall.com/" target="_blank">
+									<img alt="天猫商城" src="https://img.alicdn.com/tfs/TB1XlF3RpXXXXc6XXXXXXXXXXXX-16-16.png">
+									<span class="title">天猫商城</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.jd.com/" target="_blank">
+									<img alt="京东" src="https://www.jd.com/favicon.ico">
+									<span class="title">京东</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1"><!-- 视频 -->
 						<div class="sub-title">
 							<a>视频</a>
 						</div>
@@ -217,20 +284,7 @@
 							</li>
 						</ul>
 					</div>
-					<div class="sub-grid1">
-						<div class="sub-title">
-							<a>导航</a>
-						</div>
-						<ul class="list clearfix">
-							<li>
-								<a href="http://www.baidu.com/" target="_blank">
-									<img alt="百度" src="http://www.baidu.com/favicon.ico">
-									<span class="title">百度</span>
-								</a>
-							</li>
-						</ul>
-					</div>
-					<div class="sub-grid1">
+					<div class="sub-grid1"><!-- 博客 -->
 						<div class="sub-title">
 							<a>博客</a>
 						</div>
@@ -279,15 +333,408 @@
 							</li>
 						</ul>
 					</div>
-					<div class="sub-grid1">
+					<div class="sub-grid1"><!-- 手机 -->
 						<div class="sub-title">
-							<a>生活</a>
+							<a>手机</a>
 						</div>
 						<ul class="list clearfix">
 							<li>
-								<a href="http://www.12306.cn/mormhweb/" target="_blank">
-									<img alt="12306" src="http://www.12306.cn/mormhweb/images/favicon.ico">
-									<span class="title">12306</span>
+								<a href="https://www.apple.com/cn/iphone/" target="_blank">
+									<img alt="iPhone" src="https://www.apple.com/favicon.ico">
+									<span class="title">iPhone</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.vmall.com/" target="_blank">
+									<img alt="华为" src="https://www.vmall.com/favicon.ico">
+									<span class="title">华为</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.samsung.com/" target="_blank">
+									<img alt="三星" src="https://cdn.samsung.com/etc/designs/smg/global/imgs/favicon.ico">
+									<span class="title">三星</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.mi.com/" target="_blank">
+									<img alt="小米" src="http://www.mi.com/favicon.ico">
+									<span class="title">小米</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.meizu.com/" target="_blank">
+									<img alt="魅族" src="https://www3.res.meizu.com/static/cn/global/images/favicon_19fbf41.ico">
+									<span class="title">魅族</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.oppo.com/" target="_blank">
+									<img alt="oppo" src="http://www.oppo.com/favicon.ico">
+									<span class="title">oppo</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.vivo.com.cn/" target="_blank">
+									<img alt="vivo" src="http://www.vivo.com.cn/favicon.ico">
+									<span class="title">vivo</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.gionee.com/" target="_blank">
+									<img alt="金立" src="https://www.gionee.com/favicon.ico">
+									<span class="title">金立</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.smartisan.com/" target="_blank">
+									<img alt="锤子" src="http://static.smartisanos.cn/common/img/favicon.ico">
+									<span class="title">锤子</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.lemall.com/phone.html" target="_blank">
+									<img alt="乐视" src="https://www.lemall.com/htmlResource/images/favicon.ico">
+									<span class="title">乐视</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1"><!-- 直播 -->
+						<div class="sub-title">
+							<a>直播</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="https://www.douyu.com/" target="_blank">
+									<img alt="斗鱼TV" src="https://www.douyu.com/favicon.ico">
+									<span class="title">斗鱼TV</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.panda.tv/" target="_blank">
+									<img alt="熊猫TV" src="https://www.panda.tv/favicon.ico">
+									<span class="title">熊猫TV</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.huya.com/" target="_blank">
+									<img alt="虎牙TV" src="http://www.huya.com/favicon.ico">
+									<span class="title">虎牙TV</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.quanmin.tv/" target="_blank">
+									<img alt="全民TV" src="https://static.quanmin.tv/public/common/pub/img/favicon.ico">
+									<span class="title">全民TV</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.zhanqi.tv/" target="_blank">
+									<img alt="战旗TV" src="http://www.zhanqi.tv/favicon.ico">
+									<span class="title">战旗TV</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.huomao.com/" target="_blank">
+									<img alt="火猫TV" src="https://www.huomao.com/favicon.ico">
+									<span class="title">火猫TV</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.twitch.tv/" target="_blank">
+									<img alt="Twitch" src="https://www.twitch.tv/favicon.ico">
+									<span class="title">Twitch</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://longzhu.com/" target="_blank">
+									<img alt="龙珠TV" src="http://longzhu.com/favicon.ico">
+									<span class="title">龙珠TV</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.inke.cn/" target="_blank">
+									<img alt="映客TV" src="http://static.inke.cn/s/images/favicon.ico">
+									<span class="title">映客TV</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1"><!-- 音乐 -->
+						<div class="sub-title">
+							<a>音乐</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://music.163.com/" target="_blank">
+									<img alt="网易云音乐" src="http://s1.music.126.net/music.ico?v1">
+									<span class="title">网易云音乐</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.yinyuetai.com/" target="_blank">
+									<img alt="音悦台" src="http://www.yinyuetai.com/favicon.ico">
+									<span class="title">音悦台</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://y.qq.com/" target="_blank">
+									<img alt="QQ音乐" src="https://y.qq.com/favicon.ico">
+									<span class="title">QQ音乐</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://music.baidu.com/" target="_blank">
+									<img alt="百度音乐" src="http://music.baidu.com/static/images/favicon.ico">
+									<span class="title">百度音乐</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.kugou.com/" target="_blank">
+									<img alt="酷狗音乐" src="http://www.kugou.com/root/favicon.ico">
+									<span class="title">酷狗音乐</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.kuwo.cn/" target="_blank">
+									<img alt="酷我音乐" src="http://image.kuwo.cn/website/favicon.ico">
+									<span class="title">酷我音乐</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.xiami.com/" target="_blank">
+									<img alt="虾米音乐" src="http://g.alicdn.com/de/music-static/favicon.ico">
+									<span class="title">虾米音乐</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://douban.fm/" target="_blank">
+									<img alt="豆瓣电台" src="https://img3.doubanio.com/f/fm/55cc7ebd1777d5101a82d7d6ce47ffc5e114131d/pics/fm/san_favicon.ico">
+									<span class="title">豆瓣电台</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.migu.cn/music.html" target="_blank">
+									<img alt="咪咕音乐" src="http://www.migu.cn/favicon.ico">
+									<span class="title">咪咕音乐</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://music.weibo.com/" target="_blank">
+									<img alt="新浪乐库" src="http://music.weibo.com/favicon.ico">
+									<span class="title">新浪乐库</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1"><!-- 小说 -->
+						<div class="sub-title">
+							<a>小说</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="https://www.qidian.com/" target="_blank">
+									<img alt="起点中文网" src="https://www.qidian.com/favicon.ico">
+									<span class="title">起点中文网</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.hongxiu.com/" target="_blank">
+									<img alt="红袖添香" src="./resource/images/ico/hongxiu_favicon.ico">
+									<span class="title">红袖添香</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://yuedu.163.com/" target="_blank">
+									<img alt="网易云阅读" src="http://yuedu.163.com/favicon.ico">
+									<span class="title">网易云阅读</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://book.qq.com/" target="_blank">
+									<img alt="QQ阅读" src="http://img1.chuangshi.qq.com/book/p1/ico/bookqq.ico">
+									<span class="title">QQ阅读</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://yuedu.baidu.com/" target="_blank">
+									<img alt="百度阅读" src="https://www.baidu.com/cache/icon/favicon.ico">
+									<span class="title">百度阅读</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.xxsy.net/" target="_blank">
+									<img alt="潇湘书院" src="http://www.xxsy.net/favicon.ico">
+									<span class="title">潇湘书院</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.qdmm.com/" target="_blank">
+									<img alt="起点女生网" src="https://qidian.gtimg.com/qd/favicon/qdmm_icon.6bb22.ico">
+									<span class="title">起点女生网</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1"><!-- 漫画 -->
+						<div class="sub-title">
+							<a>漫画</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://baozoumanhua.com/" target="_blank">
+									<img alt="暴走漫画" src="http://baozoumanhua.com/favicon.ico">
+									<span class="title">暴走漫画</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.buka.cn/" target="_blank">
+									<img alt="布卡漫画" src="http://c-r7.ibuka.cn/static/www.buka.cn/img/ico.ico">
+									<span class="title">布卡漫画</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1"><!--  OJ  -->
+						<div class="sub-title">
+							<a>OJ</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://poj.org/" target="_blank">
+									<img alt="POJ" src="http://poj.org/poj.ico">
+									<span class="title">POJ</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://acm.hdu.edu.cn/" target="_blank">
+									<img alt="HDU" src="http://acm.hdu.edu.cn/favicon.ico">
+									<span class="title">HDU</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.nbuoj.com/" target="_blank">
+									<img alt="NBU" src="http://www.nbuoj.com/v8.8/Images/bg_block.jpg">
+									<span class="title">NBU</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1"><!-- 手游 -->
+						<div class="sub-title">
+							<a>手游</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://hy.163.com/" target="_blank">
+									<img alt="荒野行动" src="http://hy.163.com/favicon.ico">
+									<span class="title">荒野行动</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://pvp.qq.com/" target="_blank">
+									<img alt="王者荣耀" src="http://pvp.qq.com/favicon.ico">
+									<span class="title">王者荣耀</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://t2.163.com/" target="_blank">
+									<img alt="终结者2" src="./resource/images/ico/t2_favicon.ico">
+									<span class="title">终结者2</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://cfm.qq.com/" target="_blank">
+									<img alt="枪战王者" src="http://cfm.qq.com/favicon.ico">
+									<span class="title">枪战王者</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://qjnn.qq.com/" target="_blank">
+									<img alt="奇迹暖暖" src="http://qjnn.qq.com/favicon.ico">
+									<span class="title">奇迹暖暖</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.benghuai.com/" target="_blank">
+									<img alt="崩坏学园2" src="http://www.benghuai.com/favicon.ico">
+									<span class="title">崩坏学园2</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1"><!-- 网游 -->
+						<div class="sub-title">
+							<a>网游</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://jx3.xoyo.com/" target="_blank">
+									<img alt="剑网3" src="http://jx3.xoyo.com/favicon.ico">
+									<span class="title">剑网3</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://dnf.qq.com/" target="_blank">
+									<img alt="DNF" src="http://dnf.qq.com/favicon.ico">
+									<span class="title">DNF</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://bns.qq.com/" target="_blank">
+									<img alt="剑灵" src="http://bns.qq.com/favicon.ico">
+									<span class="title">剑灵</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1"><!-- 单机 -->
+						<div class="sub-title">
+							<a>单机</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://www.gamersky.com/" target="_blank">
+									<img alt="游民星空" src="http://www.gamersky.com/favicon.ico">
+									<span class="title">游民星空</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://www.ptbus.com/" target="_blank">
+									<img alt="口袋巴士" src="http://www.ptbus.com/favicon.ico">
+									<span class="title">口袋巴士</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://store.steampowered.com/" target="_blank">
+									<img alt="Steam" src="http://store.steampowered.com/favicon.ico">
+									<span class="title">Steam</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="sub-grid1"><!-- 竞技 -->
+						<div class="sub-title">
+							<a>竞技</a>
+						</div>
+						<ul class="list clearfix">
+							<li>
+								<a href="http://lol.qq.com/" target="_blank">
+									<img alt="英雄联盟" src="http://lol.qq.com/favicon.ico">
+									<span class="title">英雄联盟</span>
+								</a>
+							</li>
+							<li>
+								<a href="http://codol.qq.com/" target="_blank">
+									<img alt="使命召唤" src="http://codol.qq.com/favicon.ico">
+									<span class="title">使命召唤</span>
+								</a>
+							</li>
+							<li>
+								<a href="https://csol.tiancity.com/" target="_blank">
+									<img alt="CS:OL" src="https://csol.tiancity.com/favicon.ico">
+									<span class="title">CS:OL</span>
 								</a>
 							</li>
 						</ul>
@@ -296,7 +743,7 @@
 			</div>
 		</div>
 	</body>
-	<script src="./resource/js/require.js" defer async="true" data-main="./main.js"data-main="js/main"></script>
+	<script src="./resource/js/require.js" defer async="true" data-main="./resource/js/main.js" data-main="js/main"></script>
 	<script>
 		window.onload = function() {
 			
